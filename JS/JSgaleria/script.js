33a0d2dd33f56f96f64cd1d2c5cdd76aaa476e51
Miguel Esteban <miguelestebn@gmail.com>
2025-01-14 08:21:10 +0100

    a

--- JS/JSgaleria/script.js
+++ JS/JSgaleria/script.js
@@ -1,8 +1,8 @@
-const animales= [
+const datos= [
     ["Dingo","dingo.jpg","Canino"],
     ["León","leon.jpg","Felino"],
     ["Zebra","zebra.jpg","Equino"],
-    ["Aguila","zebra.jpg","Ave"],
+    ["Aguila","aguila.jpg","Ave"],
     ["Cobra","zebra.jpg","Reptil"],
     ["Tiburón","zebra.jpg","Pez"]
 ]
@@ -11,14 +11,14 @@ const carpetaimg="img/";
 let indice=0;
 
 function sig(){
-    document.querySelector("img").src=carpetaimg+animales[indice][1];
-document.querySelector("img").alt=animales[indice][0];
-document.querySelector("h1").innerText=animales[indice][0];
-document.querySelector("p").innerText=animales[indice][2];
+    document.querySelector("img").src=carpetaimg+datos[indice][1];
+document.querySelector("img").alt=datos[indice][0];
+document.querySelector("h1").innerText=datos[indice][0];
+document.querySelector("p").innerText=datos[indice][2];
 document.querySelector("h2").innerText=indice;
 indice++
 
-if(indice>animales.length-1){
+if(indice>datos.length-1){
     indice=0;
 }
 }
